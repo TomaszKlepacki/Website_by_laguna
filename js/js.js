a17f558b13ea714b416e98e72dbb6f8fc28c80c5
Tomasz Klepacki <tomaszklepacki@op.pl>
2021-06-24 22:22:54 +0200

    Add new files

--- js/js.js
+++ js/js.js
@@ -1,3 +1,12 @@
-const button = document.querySelector('.btn btn--white btn--animated');
-
-button.addEventListener('click', function())
+(function (doc)
+{
+  var display = doc.getElementById('navigation');
+  
+ // console.log(display);
+  display.addEventListener('click', function(ev)
+  {
+    var thatUl = ev.target.querySelector('#ul');
+      //console.log(thatUl.classList);
+    thatUl.classList.toggle('show');
+  }, false);
+}(document))
